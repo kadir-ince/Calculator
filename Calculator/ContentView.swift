b6c68454125d6fb2c72383eeb64a943c0c80c97e
Kadir Ince <kadirincedev@gmail.com>
2020-08-24 17:32:03 +0300

    Created Number & Operation Buttons UI Template

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { _ in
+            VStack {
+                Spacer()
+                buttonRows(for: ["7","8","9"], operation: .multiplication)
+                buttonRows(for: ["4","5","6"], operation: .substraction)
+                buttonRows(for: ["1","2","3"], operation: .addition)
+            }
+            .font(.custom("Arial Rounded MT Bold", size: 26))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+let screen = UIScreen.main.bounds
+
+enum Operation {
+    case addition
+    case substraction
+    case multiplication
+    case division
+
+    var displayedString: String {
+        switch self {
+        case .addition:
+            return "+"
+        case .substraction:
+            return "-"
+        case .multiplication:
+            return "*"
+        case .division:
+            return "/"
+        }
+    }
+}
+
+func buttonRows(for numbers: [String], operation: Operation) -> some View {
+    HStack{
+        ForEach(numbers, id: \.self) { number in
+            Button(action: {}) {
+                Text(number)
+                    .frame(width: screen.width/4.5, height: screen.height * 0.1)
+                    .foregroundColor(.gray)
+            }
+        }
+        Button(action: {}) {
+            Text(operation.displayedString)
+                .frame(width: screen.width/4.5, height: screen.height * 0.1)
+                .foregroundColor(.red)
+        }
     }
 }
