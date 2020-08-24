44720f05679f7eeedda21d5934e6dab23aae4d28
Kadir Ince <kadirincedev@gmail.com>
2020-08-24 18:12:54 +0300

    Show String on the Buttons

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var typedString = "0"
+    @State var typedString = "0"
     @Environment(\.colorScheme) var colorScheme
-    
+
     var body: some View {
         GeometryReader { _ in
             VStack {
@@ -23,7 +23,7 @@ struct ContentView: View {
                         .padding(.trailing, screen.width * 0.085)
                 }
                 Divider().background(Color.blue)
-                topButtons()
+                topButtonRow()
                 buttonRows(for: ["7", "8", "9"], operation: .multiplication)
                 buttonRows(for: ["4", "5", "6"], operation: .substraction)
                 buttonRows(for: ["1", "2", "3"], operation: .addition)
@@ -33,12 +33,81 @@ struct ContentView: View {
             .padding(.leading, screen.width * 0.02)
         }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+
+    func topButtonRow() -> some View {
+        HStack {
+            Button(action: {}) {
+                Text("")
+                    .frame(width: screen.width/4.5, height: screen.height * 0.1)
+            }
+            Button(action: {}) {
+                Text("")
+                    .frame(width: screen.width/4.5, height: screen.height * 0.1)
+            }
+            Button(action: {}) {
+                Text("C")
+                    .frame(width: screen.width/4.5, height: screen.height * 0.1)
+                    .foregroundColor(Color("ClearButton"))
+            }
+            Button(action: {}) {
+                Text("/")
+                    .frame(width: screen.width/4.5, height: screen.height * 0.1)
+                    .foregroundColor(.red)
+            }
+        }
+    }
+
+    func buttonRows(for numbers: [String], operation: Operation?) -> some View {
+        HStack {
+            ForEach(numbers, id: \.self) { number in
+                Button(action: {
+                    tapped(number: number)
+                }) {
+                    Text(number)
+                        .frame(width: screen.width/4.5, height: screen.height * 0.1)
+                        .foregroundColor(.gray)
+                }
+            }
+            if operation != nil {
+                Button(action: {}) {
+                    Text(operation!.displayedString)
+                        .frame(width: screen.width/4.5, height: screen.height * 0.1)
+                        .foregroundColor(.red)
+                }
+            } else {
+                Button(action: {}) {
+                    Text("=")
+                        .frame(width: screen.width/4.5, height: screen.height * 0.1)
+                        .background(Color.red)
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                        .shadow(radius: 10, x: 5, y: 6)
+                }
+            }
+        }
+    }
+    
+    func tapped(number: String) {
+        if number == "0" || number == "00" {
+            guard typedString != "0" else { return }
+            typedString += number
+        } else if  number == "."{
+            guard typedString != "." else {return}
+            typedString += number
+        } else {
+            if  typedString == "0" {
+                 typedString = number
+            } else {
+                 typedString += number
+            }
+        }
     }
 }
 
@@ -60,57 +129,6 @@ enum Operation {
             return "*"
         case .division:
             return "/"
-        }
-    }
-}
-
-func buttonRows(for numbers: [String], operation: Operation?) -> some View {
-    HStack {
-        ForEach(numbers, id: \.self) { number in
-            Button(action: {}) {
-                Text(number)
-                    .frame(width: screen.width/4.5, height: screen.height * 0.1)
-                    .foregroundColor(.gray)
-            }
-        }
-        if operation != nil {
-            Button(action: {}) {
-                Text(operation!.displayedString)
-                    .frame(width: screen.width/4.5, height: screen.height * 0.1)
-                    .foregroundColor(.red)
-            }
-        } else {
-            Button(action: {}) {
-                Text("=")
-                    .frame(width: screen.width/4.5, height: screen.height * 0.1)
-                    .background(Color.red)
-                    .clipShape(Circle())
-                    .foregroundColor(.white)
-                    .shadow(radius: 10, x: 5, y: 6)
-            }
-        }
-    }
-}
-
-func topButtons() -> some View {
-    HStack {
-        Button(action: {}) {
-            Text("")
-                .frame(width: screen.width/4.5, height: screen.height * 0.1)
-        }
-        Button(action: {}) {
-            Text("")
-                .frame(width: screen.width/4.5, height: screen.height * 0.1)
-        }
-        Button(action: {}) {
-            Text("C")
-                .frame(width: screen.width/4.5, height: screen.height * 0.1)
-                .foregroundColor(Color("ClearButton"))
-        }
-        Button(action: {}) {
-            Text("/")
-                .frame(width: screen.width/4.5, height: screen.height * 0.1)
-                .foregroundColor(.red)
         }
     }
 }
